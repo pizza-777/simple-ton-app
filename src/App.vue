696debd60333c42cabfc646dc67540fcc28c81fd
pizza-777 <doctor4891@gmail.com>
2023-10-06 15:59:10 +0300

    clean up

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style>
-/* @font-face {
-  src: local("Robus"),
-   url(./assets/font/Robus.otf) format("opentype");
-} */
-input {
-  background-color: rgb(93, 93, 97, 0.3) !important;
-  color: rgba(255, 255, 255, 0.9) !important;
-}
-#title {
- /* font-family: Robus; */
-}
-</style>
